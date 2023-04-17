f3385395dfd64a12f6f2ff1d18ba7a6f797bbc84
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-17 13:42:04 -0700

    finalized the verifyEmail controller

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -41,9 +41,23 @@ const register = async (req, res) => {
 const verifyEmail = async (req, res) => {
   const { verificationToken, email } = req.body;
 
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Authentication failed");
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new UnauthenticatedError("Authentication failed");
+  }
+
+  user.isVerified = true;
+  user.verified = Date.now();
+  user.verificationToken = "";
+
+  await user.save();
+
   res.status(StatusCodes.OK).json({
-    verificationToken,
-    email,
+    msg: "Email verified!",
   });
 };
 
