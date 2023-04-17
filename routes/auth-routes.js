58519d05663443c98c7efed949df207f1e338de8
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-17 13:14:26 -0700

    created a /verify-email auth route

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { register, login, logout } = require("../controllers/auth-controllers");
+const {
+  register,
+  login,
+  logout,
+  verifyEmail,
+} = require("../controllers/auth-controllers");
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
+router.route("/verify-email").post(verifyEmail);
 
 module.exports = router;
