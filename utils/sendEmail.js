7426f3a72ec68bcb6bb42d873e22c9b52710a24d
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-18 15:20:17 -0700

    refactored sendEmail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 const nodemailerConfig = require("./nodemailerConfig");
 
-const sendEmail = async () => {
+const sendEmail = async ({ to, subject, html }) => {
   let testAccount = await nodemailer.createTestAccount();
 
   const transporter = nodemailer.createTransport(nodemailerConfig);
