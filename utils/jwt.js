1708bba125961dd028094a8fdacd3796c0f1d788
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-19 16:37:12 -0700

    refactored the attachCookiesToResponse function for 2 cookies

--- utils/jwt.js
+++ utils/jwt.js
@@ -8,14 +8,24 @@ const createJWT = ({ payload }) => {
 
 const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 
-const attachCookiesToResponse = ({ res, tokenPayload }) => {
-  const token = createJWT({ payload: tokenPayload });
+const attachCookiesToResponse = ({ res, tokenPayload, refreshToken }) => {
+  const accessTokenJWT = createJWT({ payload: { tokenPayload } });
+  const refreshTokenJWT = createJWT({
+    payload: { tokenPayload, refreshToken },
+  });
 
-  const oneDay = 1000 * 60 * 60 * 24;
+  const tenMin = 1000 * 60 * 10;
+  const oneMonth = 1000 * 60 * 60 * 24 * 30;
 
-  res.cookie("token", token, {
+  res.cookie("accessToken", accessTokenJWT, {
     httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
+    expires: new Date(Date.now() + tenMin),
+    secure: process.env.NODE_ENV === "production", // to secure the cookies from modifying in the browser
+    signed: true,
+  });
+  res.cookie("refreshToken", refreshTokenJWT, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneMonth),
     secure: process.env.NODE_ENV === "production", // to secure the cookies from modifying in the browser
     signed: true,
   });
