efee17b5e4f3f415500912af553ee5ccaabbdf00
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-18 17:53:50 -0700

    fixed typos

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -32,19 +32,18 @@ const register = async (req, res) => {
     verificationToken,
   }); // to secure the role (by preventing the user to register as admin)
 
-  const origin = "http://localhost:3000"; //! change the url after pushing the app into production (see the example below)// const newOrigin = 'https://react-node-user-workflow-front-end.netlify.app';
+  const origin = 'http://localhost:3000';
   // const newOrigin = 'https://react-node-user-workflow-front-end.netlify.app';
-  /*
-  ! In case you need to configure a custom url (not a hardcoded one)
-  const tempOrigin = req.get('origin');
-  const protocol = req.protocol;
-  const host = req.get('host');
-  const forwardedHost = req.get('x-forwarded-host');
-  const forwardedProtocol = req.get('x-forwarded-proto');
-  */
+
+  // const tempOrigin = req.get('origin');
+  // const protocol = req.protocol;
+  // const host = req.get('host');
+  // const forwardedHost = req.get('x-forwarded-host');
+  // const forwardedProtocol = req.get('x-forwarded-proto');
+
   await sendVerificationEmail({
-    email: user.email,
     name: user.name,
+    email: user.email,
     verificationToken: user.verificationToken,
     origin,
   });
--- utils/sendVerficationEmail.js
+++ utils/sendVerficationEmail.js
@@ -1,4 +1,28 @@
-const sendEmail = require("./sendEmail");
+// const sendEmail = require("./sendEmail");
+
+// const sendVerificationEmail = async ({
+//   name,
+//   email,
+//   verificationToken,
+//   origin,
+// }) => {
+//   // /user/verify-email is a front-end route
+//   const verifyEmailUrl = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`;
+//   const message = `<p>Please confirm your email by clicking on the following link: <a href="${verifyEmailUrl}">Verify Email</a></p>`;
+
+//   //   again, no await because we invoke this function inside of the register controller
+//   return sendEmail({
+//     to: email,
+//     subject: "Email Confirmation",
+//     html: `<h4>Hello ${name}</h4>
+//     ${message}
+//     `,
+//   });
+// };
+
+// module.exports = sendVerificationEmail;
+
+const sendEmail = require('./sendEmail');
 
 const sendVerificationEmail = async ({
   name,
@@ -6,15 +30,15 @@ const sendVerificationEmail = async ({
   verificationToken,
   origin,
 }) => {
-  // /user/verify-email is a front-end route
-  const verifyEmailUrl = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`;
-  const message = `<p>Please confirm your email by clicking on the following link: <a href="${verifyEmailUrl}">Verify Email</a></p>`;
+  const verifyEmail = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`;
 
-  //   again, no await because we invoke this function inside of the register controller
+  const message = `<p>Please confirm your email by clicking on the following link : 
+  <a href="${verifyEmail}">Verify Email</a> </p>`;
+
   return sendEmail({
     to: email,
-    subject: "Email Confirmation",
-    html: `<h4>Hello ${name}</h4>
+    subject: 'Email Confirmation',
+    html: `<h4> Hello, ${name}</h4>
     ${message}
     `,
   });
