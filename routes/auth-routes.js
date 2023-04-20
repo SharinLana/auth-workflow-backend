68ce89a01210842411e48baaa0fdcd34db5b593f
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-20 15:24:22 -0700

    created post routes for resetPassword and forgotPassword

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -5,6 +5,8 @@ const {
   login,
   logout,
   verifyEmail,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/auth-controllers");
 
 const router = express.Router();
@@ -13,5 +15,7 @@ router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").delete(authenticateUser, logout);
 router.route("/verify-email").post(verifyEmail);
+router.route("/reset-password").post(resetPassword);
+router.route("/forgot-password").post(forgotPassword);
 
 module.exports = router;
